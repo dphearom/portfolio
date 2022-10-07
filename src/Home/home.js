9fd30969215675028ffa81ecb11a76facbb499bc
U-CARBON\duong <rduong@wesleyan.edu>
2022-10-07 14:57:10 -0400

    Adding Tailwind and React Router

---
+++ src/Home/home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-full mx-auto">
+        <h1 className="text-4xl text-center">Testing The homepage</h1>
+      </div>
+    </>
+  );
+}
